765d86191b2d9ae1ac06d07ed53a09cab0382f30
Farid <farid.hammouten@gmail.com>
2020-11-11 08:39:06 +0900

    add mission 2 = refactored

--- 02-Add/Add.js
+++ 02-Add/Add.js
@@ -5,11 +5,7 @@ console.log(add(1,2));
 console.log(add(3,2));
 
 // Mission 2
-const add2 = (...numbers) => {
-  let total = 0;
-  numbers.forEach((number) =>  total += number);
-  return total;
-}
+const add2 = (...numbers) => numbers.reduce((total, currentValue) => total + currentValue)
 
 console.log(add2(1,2,3,4,5));
 console.log(add2(2,3))
